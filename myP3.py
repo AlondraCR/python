7f061fded86b1010341ed5855e960b0ee56771c6
Alondra <acr020800@gmail.com>
2022-08-09 01:18:53 -0500

    P2 (mySS) sumas, P3 Escribir y leer archivos, .txt el archivo para la P3

---
+++ myP3.py
@@ -0,0 +1,13 @@
+#escribir
+#f = open('myFile1.txt','w')
+#f.write("hello")
+with open('myFile1.txt', 'w') as myFile:
+	print("Hello! jeje",file=myFile)
+
+
+#leer
+#f = open('myFile1.txt','r') 
+#f.read()
+with open('myFile1.txt', 'r') as myfile:
+	data = myfile.read()
+	print(data)
--- mySS.py
+++ mySS.py
@@ -1,1 +1,8 @@
-Holii
+#Holii
+
+x = int(input("Escribe un numero: "))
+y = int(input("Escribe otro numero: "))
+suma = x+y
+#print("Suma: "+ str(x+y))
+#print("Suma: "+ suma)
+print("Suma: ", x+y)
